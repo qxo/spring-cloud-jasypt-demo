05b62009153f5ba00eff19598965e0aad134db1a
qxo <49526356@qq.com>
2021-08-13 23:18:26 +0800

    feat: init

---
+++ src/main/java/demo/DemoSpringApplication.java
@@ -0,0 +1,49 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.PropertySource;
+
+import com.ulisesbocchio.jasyptspringboot.EncryptablePropertyFilter;
+import com.ulisesbocchio.jasyptspringboot.EncryptablePropertyResolver;
+import com.ulisesbocchio.jasyptspringboot.environment.StandardEncryptableEnvironment;
+
+@SpringBootApplication
+public class DemoSpringApplication {
+
+    public static void main(String[] args) {
+        // SpringApplication.run(DemoSpringApplication.class, args);
+        SpringApplication app = new SpringApplication(DemoSpringApplication.class);
+        //CustomJasyptConfig config = new CustomJasyptConfig();
+        StandardEncryptableEnvironment environment = new StandardEncryptableEnvironment();
+
+        app.setEnvironment(environment);
+        app.run(args);
+    }
+
+    // @Configuration
+    public static class CustomJasyptConfig {
+
+        @Bean(name = "encryptablePropertyFilter")
+        public EncryptablePropertyFilter getEncryptablePropertyFilter() {
+            return new EncryptablePropertyFilter() {
+                public boolean shouldInclude(PropertySource<?> source, String name) {
+                    return name.endsWith("password") || name.endsWith("pwd");
+                }
+            };
+        }
+
+        @Bean(name = "encryptablePropertyResolver")
+        public EncryptablePropertyResolver getEncryptablePropertyResolver() {
+            return new EncryptablePropertyResolver() {
+
+                @Override
+                public String resolvePropertyValue(String value) {
+                    return value;
+                }
+            };
+
+        }
+    }
+}
